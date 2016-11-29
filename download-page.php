bf74eb0949c1943f3999508b2f3c972a70b4b10e
Zoffix Znet <cpan@zoffix.com>
2016-11-29 15:13:28 -0500

    Update look

--- download-page.php
+++ download-page.php
@@ -37,7 +37,7 @@
         if ($bytes == 0)
             return "0.00 B";
 
-        $s = array('B', 'KB', 'MB', 'GB', 'TB', 'PB');
+        $s = array(' B', ' KB', ' MB', ' GB', ' TB', ' PB');
         $e = floor(log($bytes, 1024));
 
         return round($bytes/pow(1024, $e), 2).$s[$e];
